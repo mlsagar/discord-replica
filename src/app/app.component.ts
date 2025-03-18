68853799e9936dd9f102b826707df57a498205d8
mlsagar <sagar.maharjan617571@outlook.com>
2025-03-18 11:42:23 -0500

    Add: ngx-clerk package for the authentication in appliation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonDirective } from './shared/ui/button.directive';
+import { ClerkService, ClerkSignInComponent } from 'ngx-clerk';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonDirective],
+  imports: [RouterOutlet, ButtonDirective, ClerkSignInComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'discord-replica';
+
+  constructor(private _clerk: ClerkService) {
+    this._clerk.__init({
+      publishableKey: 'pk_test_YmlnLXdhc3AtMjUuY2xlcmsuYWNjb3VudHMuZGV2JA'
+    })
+  }
 }
